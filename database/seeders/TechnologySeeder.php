f07fe967dedea4b0f4a46fe0fd47ae98c4528cfc
daniloeberli <danilo.eberli@gmail.com>
2023-05-23 15:25:36 +0200

    feat: technology migration, seeder

---
+++ database/seeders/TechnologySeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Technology;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class TechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $technologies = ['Js','PHP','Haskell','C++','Python','C','C#','Java','SQL'];
+
+        //Schema::disableForeignKeyConstraints();
+        Technology::truncate();
+        //Schema::enableForeignKeyConstraints();
+
+        foreach($technologies as $technology){
+            
+            $new_technology = new Technology();
+            $new_technology->name = $technology;
+            $new_technology->slug = Str::slug($new_technology->name);
+            $new_technology->save();
+        }
+    }
+}
